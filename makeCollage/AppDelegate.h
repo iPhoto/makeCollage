87834aa6f8a549b7943ba482e0281928bad942d1
Svetlana Ts <tsetsorina.sa@gmail.com>
2014-08-09 13:02:25 +0400

    Initial Commit

---
+++ makeCollage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  makeCollage
+//
+//  Created by Svetlana Tsetsorina on 09.08.14.
+//  Copyright (c) 2014 BeOriginal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
